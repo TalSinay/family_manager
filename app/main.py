71130b7883ea1b1fd29833f87dba02231bebc9a2
TalSinay <94438376+TalSinay@users.noreply.github.com>
2025-08-02 13:05:13 +0300

    change provider

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-print("hello")
+
 @app.get("/")
 def home():
     return {"message": "hello Tal!!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
